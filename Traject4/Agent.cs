21f715ebb5433ea2f56e51694fbabfb546e1338d
Sam Van Lier <sam.vanlier@hotmail.com>
2021-12-10 06:57:47 +0100

    init

---
+++ Traject4/Agent.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Linq;
+
+namespace Traject4
+{
+    public class Agent
+    {
+        private readonly Random _random;
+        private readonly double _mixFactor = 0.5;
+
+        public int m_TrajNum { get; set; }
+        public Trajectory[] m_T { get; set; }
+        public double[] m_Success { get; set; }
+        public Trajectory m_Backup { get; set; }
+        public int m_ShiftIndex { get; set; }
+
+        public Agent(int trajNum)
+        {
+            m_TrajNum = trajNum;
+            m_T = Enumerable.Repeat(new Trajectory(), trajNum).ToArray();
+            m_Success = Enumerable.Repeat((double)50.0, trajNum).ToArray();
+
+            _random = new Random();
+        }
+
+        public void PrepareShift(int shiftSD)
+        {
+            m_ShiftIndex = _random.Next(m_TrajNum);
+            var trajectory = m_T[m_ShiftIndex];
+            m_Backup = (Trajectory)trajectory.Clone(); // make a full copy of the object
+
+            trajectory.Shift(shiftSD);
+        }
+
+        public Trajectory Say()
+        {
+            var m_NoiseT = (Trajectory)m_T[m_ShiftIndex].Clone();
+
+            // Addnoise must be commented out if noise is added in distance calculation.
+            //	m_NoiseT.AddNoise( NoiseLevel);
+            m_NoiseT.AddNoise2(Program.NoiseLevel);
+
+            return m_NoiseT;
+        }
+
+        public Trajectory Imitate(Trajectory t)
+        {
+            double bestDist = -1;
+
+            Trajectory best = m_T[0];
+
+            foreach (var item in m_T)
+            {
+                var dist = t.SimpleDist(item, bestDist);
+                if (dist < bestDist)
+                {
+                    bestDist = dist;
+                    best = item;
+                }
+            }
+
+            return best;
+        }
+
+        public bool Listen(Trajectory t)
+        {
+            double bestDist = -1;
+            int best = 0;
+            for (int i = 0; i < m_TrajNum; i++)
+            {
+                var item = m_T[i];
+                var dist = t.SimpleDist(item, bestDist);
+                if (dist < bestDist)
+                {
+                    bestDist = dist;
+                    best = i;
+                }
+            }
+
+            return best == m_ShiftIndex;
+        }
+
+        internal void AcceptReject(int success)
+        {
+            if (success < m_Success[m_ShiftIndex]) // no improvement, restor original
+                m_T[m_ShiftIndex] = (Trajectory)m_Backup.Clone();
+            else
+                m_T[m_ShiftIndex].Mix(m_Backup, _mixFactor);
+
+            m_Backup = null; // clear backup
+
+            m_Success[m_ShiftIndex] = _mixFactor * success + (1 - _mixFactor) * m_Success[m_ShiftIndex];
+        }
+    }
+}
---
+++ Traject4/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using MathNet.Numerics.Distributions;
+
+namespace Traject4
+{
+    class Program
+    {
+        public readonly static int SpaceSize = 10;
+        public readonly static int TrajLength = 20;
+        public readonly static double NoiseLevel = 0.1; //todo find value in paper
+
+        private readonly static int ShiftSD = 1; // Standard deviation of shift
+
+        static void Main(string[] args)
+        {
+            var random = new Random();
+
+            int agentNum = 10;
+            int trajNum = 2; //todo find correct number in paper
+            int maxIts = 250000;
+            int testNum = 100;
+
+            // create agents
+            var agents = new Agent[agentNum];
+            for (int i = 0; i < agentNum; i++)
+                agents[i] = new Agent(trajNum);
+
+            double runAvg = 0;
+            for (int index = 0; index < maxIts; index++)
+            {
+                int shifter = random.Next(agentNum);
+                agents[shifter].PrepareShift(ShiftSD);
+
+                int success = 0;
+                for (int indey = 0; indey < agentNum; indey++)
+                {
+                    // Generate random other player and make
+                    // sure it is not equal to the original player
+                    if (indey != shifter)
+                    {
+                        for (int indez = 0; indez < testNum; indez++)
+                        {
+                            var s = agents[shifter];
+                            var y = agents[indey];
+
+                            if (s.Listen(y.Imitate(s.Say())))
+                                success++;
+                        }
+
+                    }
+                }
+
+                runAvg = runAvg * 0.999 + 0.001 * ((double)success / (testNum * (agentNum - 1)));
+                agents[shifter].AcceptReject(success);
+
+                if (index % 100 == 0)
+                    Console.WriteLine($"index {index}: runAvg={runAvg}");
+            }
+
+            Console.WriteLine("done");
+
+        }
+    }
+}
